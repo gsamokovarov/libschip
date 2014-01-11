72efeb33d0fadc7ed84122a0077cf68b3cc7d8f8
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-01-11 16:43:48 +0100

    Make the tests runnable

--- test/main.c
+++ test/main.c
@@ -1,10 +1,9 @@
+#include <stdlib.h>
 #include "test.h"
-#include "test_app.c"
 #include "test_schip.c"
 
 int main(void) {
   RUN_TEST_SUITE(schip_test_suite);
-  RUN_TEST_SUITE(app_test_suite);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
